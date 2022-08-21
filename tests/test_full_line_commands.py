598ac1800d27030342ee1d65e67325d8264aa8ef
Phil Hanna <ph1204@gmail.com>
2022-08-20 20:57:07 -0400

    Added unit tests for define and load - issue #2

--- tests/test_full_line_commands.py
+++ tests/test_full_line_commands.py
@@ -1,4 +1,6 @@
+import os
 import unittest
+import tempfile
 from io import StringIO
 
 from evaluator import Evaluator
@@ -49,3 +51,41 @@ class TestFullLineCommands(unittest.TestCase):
                 self.ev.ev("meaning-of-the-universe .")
                 output = fp.getvalue()
         self.assertIn("42", output)
+
+    def test_ev_bad_define(self):
+        with StringIO() as fp:
+            with stdout_redirected(fp):
+                self.ev.ev("define bogus")
+                output = fp.getvalue()
+        self.assertIn("No function", output)
+
+    def test_ev_good_define(self):
+        with StringIO() as fp:
+            with stdout_redirected(fp):
+                self.ev.ev("define double dup 2 *")
+                self.ev.ev("7 double .")
+                output = fp.getvalue()
+        self.assertIn("14", output)
+
+    def test_ev_bad_load(self):
+        with StringIO() as fp:
+            with stdout_redirected(fp):
+                self.ev.ev("load bogus")
+                output = fp.getvalue()
+        self.assertIn("Could not", output)
+
+    def test_ev_good_load(self):
+        _, filename = tempfile.mkstemp()
+        with open(filename, "w") as fp:
+            print(" ", file=fp)
+            print("const meaning 42", file=fp)
+            print("# quit", file=fp)
+        try:
+            with StringIO() as fp:
+                with stdout_redirected(fp):
+                    self.ev.ev(f"load {filename}")
+                    self.ev.ev("meaning .")
+                    output = fp.getvalue()
+            self.assertIn("42", output)
+        finally:
+            os.remove(filename)
