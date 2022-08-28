f4207cc98bec8cf7dba1d4b237171e38956474cc
Phil Hanna <ph1204@gmail.com>
2022-08-28 16:53:48 -0400

    Added unit test for dump_stack

---
+++ tests/test_dump.py
@@ -0,0 +1,33 @@
+import re
+from io import StringIO
+from unittest import TestCase
+
+import evaluator
+from tests import stdout_redirected
+
+
+class TestDump(TestCase):
+
+    def setUp(self) -> None:
+        self.ev = evaluator.Evaluator()
+
+    def tearDown(self) -> None:
+        del self.ev
+
+    def test_dump_stack(self):
+        with StringIO() as fpout:
+            with stdout_redirected(fpout):
+                ev = self.ev
+                ev.ev("1 5 2")
+                ev.ev(".S")
+                output = fpout.getvalue()
+        self.assertEqual("1.0\n5.0\n2.0\n", output)
+
+    def test_dump_functions(self):
+        with StringIO() as fpout:
+            with stdout_redirected(fpout):
+                ev = self.ev
+                ev.ev("define meaning 42")
+                ev.ev(".F")
+                output = fpout.getvalue()
+        self.assertIsNotNone(re.search(r"FUNCTION\s+DEFINITION.*meaning\s+42", output, re.DOTALL))
