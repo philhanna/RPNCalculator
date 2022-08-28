68bc297bcdaa68f97f99e9f0dda9b51107aa9478
Phil Hanna <ph1204@gmail.com>
2022-08-28 17:09:26 -0400

    Added unit test for dump_variables

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -1,3 +1,4 @@
+import io
 import re
 from io import StringIO
 from unittest import TestCase
@@ -31,3 +32,24 @@ class TestDump(TestCase):
                 ev.ev(".F")
                 output = fpout.getvalue()
         self.assertIsNotNone(re.search(r"FUNCTION\s+DEFINITION.*meaning\s+42", output, re.DOTALL))
+
+    def test_dump_variables(self):
+        with StringIO() as fpout:
+            with stdout_redirected(fpout):
+                ev = self.ev
+                ev.ev("var interest")
+                ev.ev("0.08 interest !")
+                ev.ev("var term")
+                ev.ev("30 term !")
+                ev.ev(".V")
+                output = fpout.getvalue()
+        fp = io.StringIO(output)
+        for i, line in enumerate(fp):
+            tokens = line.split()
+            if i == 0:
+                self.assertListEqual(["VAR", "ADDR", "VALUE"], tokens)
+            elif i == 1:
+                self.assertListEqual(["interest", "0001", "0.08"], tokens)
+            elif i == 2:
+                self.assertListEqual(["term", "0002", "30.0"], tokens)
+        fp.close()
