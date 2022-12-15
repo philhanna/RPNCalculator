8e13250de26bbe84465f005a74791316b5412b56
Phil Hanna <ph1204@gmail.com>
2022-12-15 17:55:40 -0500

    Converted test_const to pytest

--- tests/test_const.py
+++ tests/test_const.py
@@ -1,35 +1,35 @@
-import unittest
+import pytest
 from io import StringIO
 
 from evaluator import Evaluator
 from tests import stdout_redirected
 
 
-class TestConst(unittest.TestCase):
+class TestConst:
 
-    def setUp(self):
+    def setup_method(self):
         self.ev = Evaluator()
 
-    def tearDown(self):
+    def teardown_method(self):
         del self.ev
 
     def test_ev_bad_const(self):
-        with self.assertRaises(RuntimeError) as ae:
+        with pytest.raises(RuntimeError) as ae:
             self.ev.ev("const bogus")
-        self.assertIn("Invalid syntax", str(ae.exception))
+        assert "Invalid syntax" in str(ae.value)
 
     def test_ev_good_const(self):
         self.ev.ev("const meaning-of-the-universe 42")
         self.ev.ev("meaning-of-the-universe 42 =")
 
     def test_ev_bad_constp(self):
-        with self.assertRaises(RuntimeError) as ae:
+        with pytest.raises(RuntimeError) as ae:
             self.ev.ev("const bogus 47 45 ")
-        self.assertIn("Invalid syntax", str(ae.exception))
+        assert "Invalid syntax" in str(ae.value)
 
     def test_ev_good_constp(self):
         with StringIO() as fp, stdout_redirected(fp):
             self.ev.ev("const meaning-of-the-universe 21 2 *")
             self.ev.ev("meaning-of-the-universe .")
             output = fp.getvalue()
-        self.assertIn("42", output)
+        assert "42" in output
