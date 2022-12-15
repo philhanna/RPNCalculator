587682011169620cceb5244599221bf4735e737b
Phil Hanna <ph1204@gmail.com>
2022-12-15 17:57:21 -0500

    Converted test_define to pytest

--- tests/test_define.py
+++ tests/test_define.py
@@ -1,26 +1,26 @@
-import unittest
+import pytest
 from io import StringIO
 
 from evaluator import Evaluator
 from tests import stdout_redirected
 
 
-class TestDefine(unittest.TestCase):
+class TestDefine:
 
-    def setUp(self):
+    def setup_method(self):
         self.ev = Evaluator()
 
-    def tearDown(self):
+    def teardown_method(self):
         del self.ev
 
     def test_ev_bad_define(self):
-        with self.assertRaises(RuntimeError) as ae:
+        with pytest.raises(RuntimeError) as ae:
             self.ev.ev("define bogus")
-        self.assertIn("No function", str(ae.exception))
+        assert "No function" in str(ae.value)
 
     def test_ev_good_define(self):
         with StringIO() as fp, stdout_redirected(fp):
             self.ev.ev("define double dup 2 *")
             self.ev.ev("7 double .")
             output = fp.getvalue()
-        self.assertIn("14", output)
+        assert "14" in output
