8eeb5d91d55f5ac5f87d6d362d7ad5b94c3aeb43
Phil Hanna <ph1204@gmail.com>
2022-12-15 19:53:17 -0500

    Converted test_log_functions to pytest

--- tests/test_log_functions.py
+++ tests/test_log_functions.py
@@ -1,30 +1,30 @@
-import unittest
+import pytest
 
 from evaluator import Evaluator
 
 
-class TestLogFunctions(unittest.TestCase):
+class TestLogFunctions:
 
-    def setUp(self):
+    def setup_method(self):
         self.ev = Evaluator()
 
-    def tearDown(self):
+    def teardown_method(self):
         del self.ev
 
     def test_exp(self):
         self.ev.ev("2 exp")
         actual = self.ev.pop().value
         expected = 7.389056098
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
 
     def test_ln(self):
         self.ev.ev("2 ln")
         actual = self.ev.pop().value
         expected = 0.6931471805599453
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
 
     def test_log(self):
         self.ev.ev("2 log")
         actual = self.ev.pop().value
         expected = 0.301029995663981
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
