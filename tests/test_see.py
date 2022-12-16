da19eee1e1f70fa966e9be7b5877c92621531788
Phil Hanna <ph1204@gmail.com>
2022-12-15 20:00:21 -0500

    Converted test_see to pytest

--- tests/test_see.py
+++ tests/test_see.py
@@ -1,16 +1,17 @@
 from io import StringIO
-from unittest import TestCase
+
+import pytest
 
 from evaluator import Evaluator
 from tests import stdout_redirected
 
 
-class TestSee(TestCase):
+class TestSee:
 
-    def setUp(self):
+    def setup_method(self):
         self.ev = Evaluator()
 
-    def tearDown(self) -> None:
+    def teardown_method(self) -> None:
         del self.ev
 
     def test_f(self):
@@ -19,7 +20,7 @@ class TestSee(TestCase):
             self.ev.ev("see even")
             actual = out.getvalue().strip()
         expected = "function EVEN: 2 mod 0 ="
-        self.assertEqual(expected, actual)
+        assert actual == expected
 
     def test_v(self):
         self.ev.ev("var stop")
@@ -28,7 +29,7 @@ class TestSee(TestCase):
             self.ev.ev("see stop")
             actual = out.getvalue().strip()
         expected = "variable STOP"
-        self.assertIn(expected, actual)
+        assert expected in actual
 
     def test_c(self):
         self.ev.ev("const k 25")
@@ -36,11 +37,11 @@ class TestSee(TestCase):
             self.ev.ev("see k")
             actual = out.getvalue().strip()
         expected = "constant K: 25.0"
-        self.assertIn(expected, actual)
+        assert expected in actual
 
     def test_bogus(self):
-        with self.assertRaises(RuntimeError) as ae:
+        with pytest.raises(RuntimeError) as ae:
             self.ev.ev("see bogus")
-        actual = str(ae.exception)
+        actual = str(ae.value)
         expected = "SEE must be followed"
-        self.assertIn(expected, actual)
+        assert expected in actual
