a30d0c72740a76da2a18377272d530e663c365e1
Phil Hanna <ph1204@gmail.com>
2022-12-15 20:06:36 -0500

    Converted test_trigonometric_functions to pytest

--- tests/test_trigonometric_functions.py
+++ tests/test_trigonometric_functions.py
@@ -1,74 +1,72 @@
-import unittest
-
 from mpmath import e, pi, radians, sin, cos
-
+import pytest
 from evaluator import Evaluator
 
 
-class TestTrigonometricFunctions(unittest.TestCase):
+class TestTrigonometricFunctions:
 
-    def setUp(self):
+    def setup_method(self):
         self.ev = Evaluator()
 
-    def tearDown(self):
+    def teardown_method(self):
         del self.ev
 
     def test_e(self):
         self.ev.ev("e")
         expected = e
         actual = self.ev.pop().value
-        self.assertEqual(expected, actual)
+        assert actual == expected
 
     def test_todegrees(self):
         self.ev.ev("pi toDegrees")
         expected = 180
         actual = self.ev.pop().value
-        self.assertEqual(expected, actual)
+        assert actual == expected
 
     def test_toradians(self):
         self.ev.ev("90 toRadians")
         expected = pi / 2.0
         actual = self.ev.pop().value
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
 
     def test_acos(self):
         self.ev.ev("0.5 acos toDegrees")
         expected = 60
         actual = self.ev.pop().value
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
 
     def test_asin(self):
         self.ev.ev("3 sqrt 2 / asin toDegrees")
         expected = 60
         actual = self.ev.pop().value
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
 
     def test_atan(self):
         self.ev.ev("1 atan")
         actual = self.ev.pop().value
         expected = pi / 4
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
 
     def test_atan2(self):
         self.ev.ev("-1 -1 atan2")
         actual = self.ev.pop().value
         expected = -3 * pi / 4
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
 
     def test_cos(self):
         self.ev.ev("55 toRadians cos")
         actual = self.ev.pop().value
         expected = cos(radians(55))
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
 
     def test_sin(self):
         self.ev.ev("14 toRadians sin")
         actual = self.ev.pop().value
         expected = sin(radians(14))
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
 
     def test_tan(self):
         self.ev.ev("45 toRadians tan")
         actual = self.ev.pop().value
         expected = 1
-        self.assertAlmostEqual(expected, actual)
+        pytest.approx(expected, actual)
