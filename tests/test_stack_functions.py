142c2e3e5464c694f75ada5329b654c1348ae9bb
Phil Hanna <ph1204@gmail.com>
2022-12-15 20:03:58 -0500

    Converted test_stack_functions.py to pytest

--- tests/test_stack_functions.py
+++ tests/test_stack_functions.py
@@ -1,44 +1,43 @@
-import unittest
-
 from evaluator import Evaluator, NumberEntry
 
 
-class TestStackFunctions(unittest.TestCase):
+class TestStackFunctions:
 
-    def setUp(self):
+    def setup_method(self):
         self.ev = Evaluator()
 
-    def tearDown(self):
+    def teardown_method(self):
         del self.ev
 
     def test_clear(self):
         self.ev.ev("1 2 3")
-        self.assertEqual(len(self.ev.stack), 3)
+        assert len(self.ev.stack) == 3
         self.ev.ev("clear")
-        self.assertEqual(len(self.ev.stack), 0)
+        assert len(self.ev.stack) == 0
 
     def test_depth(self):
         self.ev.ev("45 11 depth")
         expected = 2
         actual = self.ev.pop().value
-        self.assertEqual(expected, actual)
+        assert actual == expected
 
     def test_drop(self):
         self.ev.ev("1 2 3 drop")
-        self.assertListEqual(self.ev.stack, [NumberEntry(1), NumberEntry(2)])
+        assert self.ev.stack == [NumberEntry(1), NumberEntry(2)]
 
     def test_dup(self):
         self.ev.ev("5 dup")
-        self.assertListEqual(self.ev.stack, [NumberEntry(5), NumberEntry(5)])
+        assert self.ev.stack == [NumberEntry(5), NumberEntry(5)]
 
     def test_over(self):
         self.ev.ev("1 2 over")
-        self.assertListEqual(self.ev.stack, [NumberEntry(1), NumberEntry(2), NumberEntry(1)])
+        assert self.ev.stack == [NumberEntry(1), NumberEntry(2), NumberEntry(1)]
 
     def test_rot(self):
         self.ev.ev("10 20 30 rot")
-        self.assertListEqual(self.ev.stack, [NumberEntry(20), NumberEntry(30), NumberEntry(10)])
+        assert self.ev.stack == [NumberEntry(20), NumberEntry(30), NumberEntry(10)]
 
     def test_swap(self):
         self.ev.ev("10 20 30 swap")
-        self.assertListEqual(self.ev.stack, [NumberEntry(10), NumberEntry(30), NumberEntry(20)])
+        assert self.ev.stack == [NumberEntry(10), NumberEntry(30), NumberEntry(20)]
+
