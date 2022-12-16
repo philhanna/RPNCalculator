4876035d5b1d220e59242be383fecdb5583074b3
Phil Hanna <ph1204@gmail.com>
2022-12-15 19:43:43 -0500

    Converted test_dump to pytest

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -1,18 +1,16 @@
-import io
 import re
 from io import StringIO
-from unittest import TestCase
 
 import evaluator
 from tests import stdout_redirected
 
 
-class TestDump(TestCase):
+class TestDump:
 
-    def setUp(self) -> None:
+    def setup_method(self) -> None:
         self.ev = evaluator.Evaluator()
 
-    def tearDown(self) -> None:
+    def teardown_method(self) -> None:
         del self.ev
 
     def test_dump_stack(self):
@@ -20,34 +18,39 @@ class TestDump(TestCase):
             ev = self.ev
             ev.ev("1 5 2")
             ev.ev(".S")
-            output = fpout.getvalue()
-        self.assertEqual("1.0\n5.0\n2.0\n", output)
+            actual = fpout.getvalue()
+        expected = "1.0\n5.0\n2.0\n"
+        assert actual == expected
 
     def test_dump_functions(self):
         with StringIO() as fpout, stdout_redirected(fpout):
             ev = self.ev
             ev.ev("define meaning 42")
             ev.ev(".F")
-            output = fpout.getvalue()
-        self.assertIsNotNone(re.search(r"FUNCTION\s+DEFINITION.*meaning\s+42", output, re.DOTALL))
+            actual = fpout.getvalue()
+        expected = r"FUNCTION\s+DEFINITION.*meaning\s+42"
+        assert re.search(expected, actual, re.DOTALL)
 
     def test_dump_functions_none_defined(self):
         with StringIO() as fpout, stdout_redirected(fpout):
             ev = self.ev
             ev.ev(".F")
-            output = fpout.getvalue()
-        self.assertEqual("", output)
+            actual = fpout.getvalue()
+        expected = ""
+        assert actual == expected
 
     def test_dump_variables_none_defined(self):
         with StringIO() as fpout, stdout_redirected(fpout):
             ev = self.ev
             ev.ev(".V")
-            output = fpout.getvalue()
-        self.assertEqual("", output)
+            actual = fpout.getvalue()
+        expected = ""
+        assert actual == expected
 
     def test_dump_constants_none_defined(self):
         with StringIO() as fpout, stdout_redirected(fpout):
             ev = self.ev
             ev.ev(".C")
-            output = fpout.getvalue()
-        self.assertEqual("", output)
+            actual = fpout.getvalue()
+        expected = ""
+        assert actual == expected
