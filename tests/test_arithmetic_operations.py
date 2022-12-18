56510c50b7997553e335d142b668821731a77953
Phil Hanna <ph1204@gmail.com>
2022-12-18 11:04:37 -0500

    Converted test_arithmetic_operations

--- tests/test_arithmetic_operations.py
+++ tests/test_arithmetic_operations.py
@@ -1,150 +1,65 @@
-from io import StringIO
-
 import pytest
+from pytest import approx
 
-from evaluator import Evaluator
-from tests import stdout_redirected
+# Tests for operations fully performed in the interpreter
+BY_COMMAND = [
+    ("2 3 +", 5),
+    ("3 5 -", -2),
+    ("2 ++", 3),
+    ("4 1+", 5),
+    ("2 --", 1),
+    ("4 1-", 3),
+    ("14 5 %", 4),
+    ("7 3 / int", 2),
+    ("9 sqrt", 3),
+    ("2 3 **", 8),
+    ("2 4 ^", 16),
+    ("10 1.8 *", approx(18)),
+    ("10 1.8 /", approx(5.55555555)),
+    ("2 1 3 / **", approx(1.259921049894873)),
+]
 
 
-class TestArithmeticOperations:
+@pytest.mark.parametrize("test_input,expected", BY_COMMAND)
+def test_by_command(ev, test_input, expected):
+    ev.ev(test_input)
+    assert ev.pop().value == expected
 
-    def setup_method(self):
-        self.ev = Evaluator()
 
-    def teardown_method(self):
-        del self.ev
+# Tests for operations performed here in the test method
+BINARY_OPERATION = [
+    ("2 3", "do_add", 5),
+    ("3 5", "do_sub", -2),
+    ("10 11", "do_mult", 110),
+]
 
-    def test_add(self):
-        ev = self.ev
-        ev.ev('2 3')
-        ev.do_add()
-        expected = 5
-        actual = ev.pop().value
-        assert actual == expected
 
-    def test_add_by_command(self):
-        ev = self.ev
-        ev.ev('2 3 +')
-        expected = 5
-        actual = ev.pop().value
-        assert actual == expected
+@pytest.mark.parametrize("test_input,fname,expected", BINARY_OPERATION)
+def test_operation(ev, test_input, fname, expected):
+    ev.ev(test_input)
+    exec(f"ev.{fname}()")
+    assert ev.pop().value == expected
 
-    def test_sub(self):
-        ev = self.ev
-        ev.ev('3 5')
-        ev.do_sub()
-        expected = -2
-        actual = ev.pop().value
-        assert actual == expected
 
-    def test_sub_by_command(self):
-        ev = self.ev
-        ev.ev('3 5 -')
-        expected = -2
-        actual = ev.pop().value
-        assert actual == expected
+# Tests which fail and produce error messages
+TESTS_WITH_ERRORS = [
+    ("10 0 /", "divide by zero"),
+    ("14 0 %", "divide by zero"),
+    ("-3 sqrt", "negative"),
+    ("-3 2 **", "Cannot exponentiate"),
+    ("0 2 **", "Cannot exponentiate"),
+]
 
-    def test_mult(self):
-        self.ev.ev("10 1.8 *")
-        expected = 18
-        actual = self.ev.pop().value
-        pytest.approx(actual, expected)
 
-    def test_div(self):
-        self.ev.ev("10 1.8 /")
-        expected = 5.55555555
-        actual = self.ev.pop().value
-        pytest.approx(actual, expected)
+@pytest.mark.parametrize("test_input,errmsg", TESTS_WITH_ERRORS)
+def tests_with_errors(ev, test_input, errmsg):
+    with pytest.raises(RuntimeError) as ae:
+        ev.ev(test_input)
+    assert errmsg in str(ae.value)
 
-    def test_div_by_zero(self):
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev("10 0 /")
-        expected = "divide by zero"
-        actual = str(ae.value)
-        assert expected in actual
 
-    def test_increment(self):
-        self.ev.ev("2 ++")
-        expected = 3
-        actual = self.ev.pop().value
-        assert actual == expected
-
-    def test_increment_1plus(self):
-        self.ev.ev("4 1+")
-        expected = 5
-        actual = self.ev.pop().value
-        assert actual == expected
-
-    def test_decrement(self):
-        self.ev.ev("2 --")
-        expected = 1
-        actual = self.ev.pop().value
-        assert actual == expected
-
-    def test_decrement_1minus(self):
-        self.ev.ev("4 1-")
-        expected = 3
-        actual = self.ev.pop().value
-        assert actual == expected
-
-    def test_mod(self):
-        self.ev.ev("14 5 %")
-        expected = 4
-        actual = self.ev.pop().value
-        assert actual == expected
-
-    def test_mod_div_by_zero(self):
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev("14 0 %")
-        errmsg = str(ae.value)
-        assert "divide by zero" in errmsg
-
-    def test_int(self):
-        self.ev.ev("7 3 / int")
-        expected = 2
-        actual = self.ev.pop().value
-        assert actual == expected
-
-    def test_sqrt(self):
-        self.ev.ev("9 sqrt")
-        expected = 3
-        actual = self.ev.pop().value
-        assert actual == expected
-
-    def test_bad_sqrt(self):
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev("-3 sqrt")
-        errmsg = str(ae.value)
-        assert "negative" in errmsg
-
-    def test_pow(self):
-        self.ev.ev("2 3 **")
-        expected = 8
-        actual = self.ev.pop().value
-        assert actual == expected
-
-    def test_pow_non_integer(self):
-        self.ev.ev("2 1 3 / **")
-        expected = 1.259921049894873
-        actual = self.ev.pop().value
-        pytest.approx(expected, actual)
-
-    def test_pow_negative_base(self):
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev("-3 2 **")
-        errmsg = str(ae.value)
-        assert "Cannot exponentiate" in errmsg
-
-    def test_pow_zero_base(self):
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev("0 2 **")
-        errmsg = str(ae.value)
-        assert "Cannot exponentiate" in errmsg
-
-    def test_empty_stack(self):
-        with StringIO() as fp, stdout_redirected(fp):
-            ev = self.ev
-            ev.ev('2')
-            ev.do_add()
-            output = fp.getvalue()
-        assert "Stack empty" in output
+def test_empty_stack(ev, capsys):
+    ev.ev("2")
+    ev.do_add()
+    captured = capsys.readouterr()
+    assert "Stack empty" in captured.out
