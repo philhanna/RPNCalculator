b8cd0182951f922f8419baa10af9591fc3f45ecc
Phil Hanna <ph1204@gmail.com>
2022-12-18 14:11:55 -0500

    Converted test_stack_functions

--- tests/test_stack_functions.py
+++ tests/test_stack_functions.py
@@ -1,43 +1,40 @@
-from evaluator import Evaluator, NumberEntry
+from evaluator import NumberEntry
 
 
-class TestStackFunctions:
+def test_clear(ev):
+    ev.ev("1 2 3")
+    assert len(ev.stack) == 3
+    ev.ev("clear")
+    assert len(ev.stack) == 0
 
-    def setup_method(self):
-        self.ev = Evaluator()
 
-    def teardown_method(self):
-        del self.ev
+def test_depth(ev):
+    ev.ev("45 11 depth")
+    expected = 2
+    actual = ev.pop().value
+    assert actual == expected
 
-    def test_clear(self):
-        self.ev.ev("1 2 3")
-        assert len(self.ev.stack) == 3
-        self.ev.ev("clear")
-        assert len(self.ev.stack) == 0
 
-    def test_depth(self):
-        self.ev.ev("45 11 depth")
-        expected = 2
-        actual = self.ev.pop().value
-        assert actual == expected
+def test_drop(ev):
+    ev.ev("1 2 3 drop")
+    assert ev.stack == [NumberEntry(1), NumberEntry(2)]
 
-    def test_drop(self):
-        self.ev.ev("1 2 3 drop")
-        assert self.ev.stack == [NumberEntry(1), NumberEntry(2)]
 
-    def test_dup(self):
-        self.ev.ev("5 dup")
-        assert self.ev.stack == [NumberEntry(5), NumberEntry(5)]
+def test_dup(ev):
+    ev.ev("5 dup")
+    assert ev.stack == [NumberEntry(5), NumberEntry(5)]
 
-    def test_over(self):
-        self.ev.ev("1 2 over")
-        assert self.ev.stack == [NumberEntry(1), NumberEntry(2), NumberEntry(1)]
 
-    def test_rot(self):
-        self.ev.ev("10 20 30 rot")
-        assert self.ev.stack == [NumberEntry(20), NumberEntry(30), NumberEntry(10)]
+def test_over(ev):
+    ev.ev("1 2 over")
+    assert ev.stack == [NumberEntry(1), NumberEntry(2), NumberEntry(1)]
 
-    def test_swap(self):
-        self.ev.ev("10 20 30 swap")
-        assert self.ev.stack == [NumberEntry(10), NumberEntry(30), NumberEntry(20)]
 
+def test_rot(ev):
+    ev.ev("10 20 30 rot")
+    assert ev.stack == [NumberEntry(20), NumberEntry(30), NumberEntry(10)]
+
+
+def test_swap(ev):
+    ev.ev("10 20 30 swap")
+    assert ev.stack == [NumberEntry(10), NumberEntry(30), NumberEntry(20)]
