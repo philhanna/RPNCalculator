744c94d27945360e431e9f36c828b9fb72b2702e
Phil Hanna <ph1204@gmail.com>
2022-12-18 13:03:35 -0500

    Converted test_define

--- tests/test_define.py
+++ tests/test_define.py
@@ -1,27 +1,14 @@
-from io import StringIO
-
 import pytest
 
-from evaluator import Evaluator
-from tests import stdout_redirected
+
+def test_ev_bad_define(ev):
+    with pytest.raises(RuntimeError) as ae:
+        ev.ev("define bogus")
+    assert "No function" in str(ae.value)
 
 
-class TestDefine:
-
-    def setup_method(self):
-        self.ev = Evaluator()
-
-    def teardown_method(self):
-        del self.ev
-
-    def test_ev_bad_define(self):
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev("define bogus")
-        assert "No function" in str(ae.value)
-
-    def test_ev_good_define(self):
-        with StringIO() as fp, stdout_redirected(fp):
-            self.ev.ev("define double dup 2 *")
-            self.ev.ev("7 double .")
-            output = fp.getvalue()
-        assert "14" in output
+def test_ev_good_define(ev, capsys):
+    ev.ev("define double dup 2 *")
+    ev.ev("7 double .")
+    output = capsys.readouterr().out
+    assert "14" in output
