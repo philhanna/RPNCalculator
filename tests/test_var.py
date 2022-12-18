e2881868a47a196b9db7b4260199d0b610dd7806
Phil Hanna <ph1204@gmail.com>
2022-12-18 14:33:15 -0500

    Converted test_var

--- tests/test_var.py
+++ tests/test_var.py
@@ -1,43 +1,37 @@
 import pytest
 
-from evaluator import Evaluator
+
+def test_store(ev):
+    ev.ev("var amount")
+    ev.ev("25 amount !")
+    ev.ev("amount")
+    expected = 1
+    actual = ev.pop()
+    assert actual == expected
 
 
-class TestVariable:
+def test_bad_store(ev):
+    with pytest.raises(RuntimeError) as ae:
+        ev.ev("4 -14 !")
+    assert "Invalid memory reference" in str(ae.value)
 
-    def setup_method(self):
-        self.ev = Evaluator()
 
-    def teardown_method(self):
-        del self.ev
+def test_fetch(ev):
+    ev.ev("var amount")
+    ev.ev("1 3 / amount !")
+    ev.ev("amount @")
+    expected = .3333333333333333
+    actual = ev.pop().value
+    pytest.approx(expected, actual)
 
-    def test_store(self):
-        self.ev.ev("var amount")
-        self.ev.ev("25 amount !")
-        self.ev.ev("amount")
-        expected = 1
-        actual = self.ev.pop()
-        assert actual == expected
 
-    def test_bad_store(self):
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev("4 -14 !")
-        assert "Invalid memory reference" in str(ae.value)
+def test_fetch_bad(ev):
+    with pytest.raises(RuntimeError) as ae:
+        ev.ev("-3 @")
+    assert "Invalid memory reference, index=-3" in str(ae.value)
 
-    def test_fetch(self):
-        self.ev.ev("var amount")
-        self.ev.ev("1 3 / amount !")
-        self.ev.ev("amount @")
-        expected = .3333333333333333
-        actual = self.ev.pop().value
-        pytest.approx(expected, actual)
 
-    def test_fetch_bad(self):
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev("-3 @")
-        assert "Invalid memory reference, index=-3" in str(ae.value)
-
-    def test_fetch_bad2(self):
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev("1000 @")
-        assert "Invalid memory reference, index=1000" in str(ae.value)
+def test_fetch_bad2(ev):
+    with pytest.raises(RuntimeError) as ae:
+        ev.ev("1000 @")
+    assert "Invalid memory reference, index=1000" in str(ae.value)
