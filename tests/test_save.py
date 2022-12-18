5e5266418229bb1c7ad7353d0fa6f433023612e7
Phil Hanna <ph1204@gmail.com>
2022-12-18 14:01:49 -0500

    Converted test_save

--- tests/test_save.py
+++ tests/test_save.py
@@ -1,39 +1,19 @@
-import tempfile
-from io import StringIO
-from pathlib import Path
-
 import pytest
 
-from evaluator import Evaluator
-from tests import stdout_redirected
 
-tmp = tempfile.gettempdir()
+def test_ev_save_no_filename(ev):
+    ev.ev("const meaning 42")
+    with pytest.raises(RuntimeError) as ae:
+        ev.ev("save")
+    assert "No file name" in str(ae.value)
 
 
-class TestSave:
-
-    def setup_method(self):
-        self.ev = Evaluator()
-
-    def teardown_method(self):
-        del self.ev
-
-    def test_ev_save_no_filename(self):
-        self.ev.ev("const meaning 42")
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev(f"save")
-        assert "No file name" in str(ae.value)
-
-    def test_ev_good_save(self):
-        filename = Path(tmp).joinpath("good_save")
-        try:
-            with StringIO() as fp, stdout_redirected(fp):
-                self.ev.ev("const meaning 42")
-                self.ev.ev("var addr")
-                self.ev.ev("7500 addr !")
-                self.ev.ev("define double dup 2 *")
-                self.ev.ev(f"save {filename}")
-                output = fp.getvalue()
-            assert "saved to" in output
-        finally:
-            filename.unlink()
+def test_ev_good_save(ev, capsys, tmp_path):
+    filename = tmp_path / "good_save"
+    ev.ev("const meaning 42")
+    ev.ev("var addr")
+    ev.ev("7500 addr !")
+    ev.ev("define double dup 2 *")
+    ev.ev(f"save {filename}")
+    output = capsys.readouterr().out
+    assert "saved to" in output
