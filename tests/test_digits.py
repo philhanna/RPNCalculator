e1310e5cf1a5f33dd78625a4c203a379e850f048
Phil Hanna <ph1204@gmail.com>
2022-12-18 13:05:47 -0500

    Converted test_digits

--- tests/test_digits.py
+++ tests/test_digits.py
@@ -1,30 +1,20 @@
-from io import StringIO
-
 import pytest
 
-from evaluator import Evaluator
-from tests import stdout_redirected
+
+def test_do_digits(ev, capsys):
+    ev.ev("digits")
+    output = capsys.readouterr().out.strip()
+    assert output.isdecimal()
 
 
-class TestDigits:
+def test_set_digits(ev, capsys):
+    ev.ev("digits 15")
+    ev.ev("pi .")
+    output = capsys.readouterr().out.strip()
+    assert "3.14159265358979" == output
 
-    def test_do_digits(self):
-        with StringIO() as fp, stdout_redirected(fp):
-            ev = Evaluator()
-            ev.ev("digits")
-            output = fp.getvalue().strip()
-        assert output.isdecimal()
 
-    def test_set_digits(self):
-        with StringIO() as fp, stdout_redirected(fp):
-            ev = Evaluator()
-            ev.ev("digits 15")
-            ev.ev("pi .")
-            output = fp.getvalue().strip()
-        assert "3.14159265358979" == output
-
-    def test_bad_digits(self):
-        ev = Evaluator()
-        with pytest.raises(RuntimeError) as ae:
-            ev.ev("digits asdf")
-        assert "'asdf'" in str(ae.value)
+def test_bad_digits(ev):
+    with pytest.raises(RuntimeError) as ae:
+        ev.ev("digits asdf")
+    assert "'asdf'" in str(ae.value)
