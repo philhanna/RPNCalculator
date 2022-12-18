6f3ce776ea78ca1b5a8bccd03b22ffc43198d296
Phil Hanna <ph1204@gmail.com>
2022-12-18 14:07:17 -0500

    Converted test_see

--- tests/test_see.py
+++ tests/test_see.py
@@ -1,47 +1,34 @@
-from io import StringIO
-
 import pytest
 
-from evaluator import Evaluator
-from tests import stdout_redirected
+
+def test_f(ev, capsys):
+    ev.ev("define even 2 mod 0 =")
+    ev.ev("see even")
+    actual = capsys.readouterr().out.strip()
+    expected = "function EVEN: 2 mod 0 ="
+    assert actual == expected
 
 
-class TestSee:
+def test_v(ev, capsys):
+    ev.ev("var stop")
+    ev.ev("false stop !")
+    ev.ev("see stop")
+    actual = capsys.readouterr().out.strip()
+    expected = "variable STOP"
+    assert expected in actual
 
-    def setup_method(self):
-        self.ev = Evaluator()
 
-    def teardown_method(self) -> None:
-        del self.ev
+def test_c(ev, capsys):
+    ev.ev("const k 25")
+    ev.ev("see k")
+    actual = capsys.readouterr().out.strip()
+    expected = "constant K: 25.0"
+    assert expected in actual
 
-    def test_f(self):
-        self.ev.ev("define even 2 mod 0 =")
-        with StringIO() as out, stdout_redirected(out):
-            self.ev.ev("see even")
-            actual = out.getvalue().strip()
-        expected = "function EVEN: 2 mod 0 ="
-        assert actual == expected
 
-    def test_v(self):
-        self.ev.ev("var stop")
-        self.ev.ev("false stop !")
-        with StringIO() as out, stdout_redirected(out):
-            self.ev.ev("see stop")
-            actual = out.getvalue().strip()
-        expected = "variable STOP"
-        assert expected in actual
-
-    def test_c(self):
-        self.ev.ev("const k 25")
-        with StringIO() as out, stdout_redirected(out):
-            self.ev.ev("see k")
-            actual = out.getvalue().strip()
-        expected = "constant K: 25.0"
-        assert expected in actual
-
-    def test_bogus(self):
-        with pytest.raises(RuntimeError) as ae:
-            self.ev.ev("see bogus")
-        actual = str(ae.value)
-        expected = "SEE must be followed"
-        assert expected in actual
+def test_bogus(ev, capsys):
+    with pytest.raises(RuntimeError) as ae:
+        ev.ev("see bogus")
+    actual = str(ae.value)
+    expected = "SEE must be followed"
+    assert expected in actual
