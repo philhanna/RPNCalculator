73a74a182eaacac1e711767f467e0e36986719f3
Phil Hanna <ph1204@gmail.com>
2022-12-18 13:58:25 -0500

    Converted test_log_functions

--- tests/test_log_functions.py
+++ tests/test_log_functions.py
@@ -1,30 +1,13 @@
 import pytest
+from pytest import approx
 
-from evaluator import Evaluator
 
-
-class TestLogFunctions:
-
-    def setup_method(self):
-        self.ev = Evaluator()
-
-    def teardown_method(self):
-        del self.ev
-
-    def test_exp(self):
-        self.ev.ev("2 exp")
-        actual = self.ev.pop().value
-        expected = 7.389056098
-        pytest.approx(expected, actual)
-
-    def test_ln(self):
-        self.ev.ev("2 ln")
-        actual = self.ev.pop().value
-        expected = 0.6931471805599453
-        pytest.approx(expected, actual)
-
-    def test_log(self):
-        self.ev.ev("2 log")
-        actual = self.ev.pop().value
-        expected = 0.301029995663981
-        pytest.approx(expected, actual)
+@pytest.mark.parametrize("test_input, expected", [
+    ("2 exp", approx(7.389056098)),
+    ("2 ln",  approx(0.6931471805599453)),
+    ("2 log", approx(0.301029995663981))
+])
+def test_log_function(ev, test_input, expected):
+    ev.ev(test_input)
+    actual = ev.pop().value
+    assert actual == expected
